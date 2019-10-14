b169651730669687adac6ded71d2771d56c551cc
Abhilash Majumder <30946547+abhilash1910@users.noreply.github.com>
2019-10-14 21:01:35 +0530

    Comments for code clarity
    Additional comments to have code clarity.

--- Example01/src/main.c
+++ Example01/src/main.c
@@ -78,12 +78,13 @@ int main(int argc, char* argv[])
     		EGL_NONE
     };
 
+	//Initialization function
     glusWindowSetInitFunc(init);
-
+	//Resizes Window per frame
     glusWindowSetReshapeFunc(reshape);
-
+	//Updates per frame
     glusWindowSetUpdateFunc(update);
-
+	//Terminate Window function
     glusWindowSetTerminateFunc(terminate);
 
     if (!glusWindowCreate("GLUS Example Window", 640, 480, GLUS_FALSE, GLUS_FALSE, eglConfigAttributes, eglContextAttributes, 0))
@@ -91,7 +92,7 @@ int main(int argc, char* argv[])
         printf("Could not create window!\n");
         return -1;
     }
-
+	//Run the current frame
     glusWindowRun();
 
     return 0;
